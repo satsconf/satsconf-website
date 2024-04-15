55bebb5a1dcffe54183fc7a8b18a9c13a8d6348b
Paula <eipvi@proton.me>
2024-04-15 13:14:36 -0300

    feat: component logo and event

---
+++ src/components/images/MeuComponenteEvento/index.jsx
@@ -0,0 +1,12 @@
+const MeuComponenteEvento = () => {
+  return (
+    <img
+      src="./satsconf-evento.png"
+      width="320"
+      height="320"
+      alt="Descrição da Imagem"
+    />
+  );
+};
+
+export default MeuComponenteEvento;
---
+++ src/components/images/MeuComponenteLogo/index.jsx
@@ -0,0 +1,9 @@
+const MeuComponenteLogo = () => {
+  return (
+    <svg width="214" height="42">
+      <image href="/satsconf-logo.svg" width="214px" height="42px" alt="" />
+    </svg>
+  );
+};
+
+export default MeuComponenteLogo;
