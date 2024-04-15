33c93fbe1665ec507ef1a080156174d0d5e1d2b9
Paula <eipvi@proton.me>
2024-04-15 13:15:16 -0300

    feat: component header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Navbar from "../navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
