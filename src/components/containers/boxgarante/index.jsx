98a9fac8a5c7c200eed721efa58b13cac8e3bcee
Paula <eipvi@proton.me>
2024-04-16 08:01:30 -0300

    feat: Box component guarantees VIP image

--- src/components/containers/boxgarante/index.jsx
+++ src/components/containers/boxgarante/index.jsx
@@ -1,51 +1,102 @@
 "use client"
 
+import Link from "next/link";
 import { FiCheckCircle } from "react-icons/fi";
-
 import MeuComponenteLogo from "@/components/images/MeuComponenteLogo";
+import MeuComponenteIgressoVip from "@/components/images/ImageLogoVip";
+import BtnCronograma from "@/components/ui/button/BtnCronograma";
+
 
 const BoxGarante = () => {
   return (
-<div className="backgroundTop">
-<div className="bgContainerGarante font-white">
+    <div className="backgroundTop">
+      <div className="bgContainerGarante font-white">
 
-      <h2 className="krona-one-regular">Garante seu ingresso agora mesmo!</h2>
-      <div className="boxGarante">
-        <div className="boxItem boxColorPrimary">
-          <span>2 A 4 NOVEMBRO</span>
-          <di className="boxLogoGarante">
-          <MeuComponenteLogo />
-          </di>
-          
-          <ul className="boxListGarante">
-            <li>
-              <FiCheckCircle size={20} color="white" /> Acesso a palestras do
-              Palco X
-            </li>
-            <li>
-              <FiCheckCircle size={20} color="white" /> Acesso a palestras do
-              Palco X
-            </li>
-            <li>
-              <FiCheckCircle size={20} color="white" /> Acesso a palestras do
-              Palco X
-            </li>
-            <li>
-              <FiCheckCircle size={20} color="white" /> Acesso a palestras do
-              Palco X
-            </li>
-          </ul>
-        </div>
-        <div className="boxItemTwo boxColorSecundary">
-          <span>2 A 6 NOVEMBRO</span>
+        <h2 className="krona-one-regular sc-fz-36">Garante seu ingresso agora mesmo!</h2>
+        <div className="boxGarante">
+          <div className="boxItem boxColorPrimary">
+            <span className="krona-one-regular ">2 A 4 NOVEMBRO</span>
+            <div className="boxLogoGarante">
+              <MeuComponenteLogo />
+            </div>
 
-          <ul>
-            <li>teste</li>
-            <li>teste</li>
-          </ul>
+            <ul className="boxListGarante">
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+
+            </ul>
+            <BtnCronograma url=""> ESGOTADO</BtnCronograma>
+          </div>
+
+          <div className="boxItemTwo boxColorSecundary">
+            <span className="krona-one-regular ">2 A 6 NOVEMBRO</span>
+            <di className="boxLogoGarante">
+              <MeuComponenteIgressoVip />
+            </di>
+
+            <ul className="boxListGarante">
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X ssssssssssssssssssssssssssssss
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>  <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+              <li>
+                <FiCheckCircle size={20} color="white" /> Acesso a palestras do
+                Palco X
+              </li>
+            </ul>
+            <div className="boxListGaranteInfo">
+              <p>*Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+            </div>
+            <BtnCronograma url=""> COMPRAR INGRESSO AGORA</BtnCronograma>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
---
+++ src/components/images/ImageLogoVip/index.jsx
@@ -0,0 +1,10 @@
+const MeuComponenteIgressoVip = () => {
+    return (
+      <svg width="214" height="42">
+        <image href="/satsconf-ingresso-vip.svg" width="214px" height="42px" alt="" />
+      </svg>
+    );
+  };
+  
+  export default MeuComponenteIgressoVip;
+  
