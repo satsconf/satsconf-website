b8ae55c363cb5459222a7768c4bef8707f1658f0
Paula <eipvi@proton.me>
2024-04-16 21:03:38 -0300

    feat: Box component cronograma index ajust

--- src/components/containers/boxcronograma/index.jsx
+++ src/components/containers/boxcronograma/index.jsx
@@ -1,33 +1,46 @@
+import SatsConf from "@/components/images/MeuComponenteSatsConf";
+import SatsParty from "@/components/images/Satsparty";
+import SatsWeek from "@/components/images/satsweek";
+
 const BoxCronograma = () => {
   return (
-    <div className="backgroundBottom">
+  
 
       <div className="bgContainerCronograma">
         {/* componente cronograma */}
         <h3 className="krona-one-regular sc-fz-36">Cronograma SATS CONF 2024</h3>
         <div className="boxCronograma">
           <div className="boxItemCronograma">
-            <span>teste</span>
-            <h1>imagem</h1>
-            <p>teste</p>
-            <span>descrição</span>
+            <h4>8 e 9 de Novembro</h4>
+            <div className="imageCronograma ">
+            <SatsConf />
+
+            </div>
+            <p>Palestras, painéis, workshops, competições, jogos, networking, comidas, bebidas, lojas e muito mais.</p>
+            <span className="krona-one-regular ">Audio | São Paulo</span>
           </div>
           <div className="boxItemCronograma ">
-            <span>teste</span>
-            <h1>imagem</h1>
-            <p>teste</p>
-            <span>descrição</span>
+            <h4>10 de Novembro</h4>
+            <div className="imageCronograma">
+            <SatsParty />
+            </div>
+         
+            <p>Uma festa exclusiva para os VIPs com ótima comida e networking com palestrantes e influenciadores.</p>
+            <span className="krona-one-regular ">Local Secreto </span>
           </div>
           <div className="boxItemCronograma">
-            <span>teste</span>
-            <h1>imagem</h1>
-            <p>teste</p>
-            <span>descrição</span>
+            <h4>4 a 10 de Novembro</h4>
+            <div className="imageCronograma">
+            <SatsWeek />
+            </div>
+         
+            <p>Uma semana inteira com vários tipos de eventos, encontros, festas e celebrações da comunidade bitcoiner.</p>
+            <span className="krona-one-regular ">Por toda São Paulo</span>
           </div>
         </div>
       </div>
 
-    </div>
+    
   )
 }
 
