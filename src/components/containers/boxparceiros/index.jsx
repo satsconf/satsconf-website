6a718fdb687bbd35564908a828fd44285eb77fda
Paula <eipvi@proton.me>
2024-04-17 12:44:52 -0300

    feat: Box component parceiros

---
+++ src/components/containers/boxparceiros/index.jsx
@@ -0,0 +1,49 @@
+const BoxParceiros = () => {
+    return (
+        <div className="bgContainerParceiros">
+            <h2 className="krona-one-regular sc-fz-36 font-white">Parceiros</h2>
+            <div className="boxParceiros">
+                <ul className="boxListParceiros">
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                    <li>
+                        {" "}
+                        <img src="empresa-x.png" alt="empresa x" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default BoxParceiros;
