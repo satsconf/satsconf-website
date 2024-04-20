6d5638cffe612f9fd4fd52afc0674c004d85554f
Paula <eipvi@proton.me>
2024-04-20 12:06:10 -0300

    fix: adjustment to the benefit component style and code formatting

--- src/components/(landingpage)/containers/boxbeneficios/index.jsx
+++ src/components/(landingpage)/containers/boxbeneficios/index.jsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic"; // Importa a função dynamic do Next.js
+
 const BtnBeneficios = dynamic(() =>
   import("@/components/(landingpage)/ui/button/BtnBeneficios/index.jsx")
 ); // Lazy loading do componente BtnBeneficios
@@ -14,26 +15,28 @@ const BoxBeneficios = () => {
   };
 
   return (
-    <div className="bgContainerBeneficios">
-      <div className="containerBotoes sc-flex-center font-white sc-gap-32">
-        {[...Array(1)].map((_, index) => (
-          <div
-            key={index}
-            className="linhaBotoes sc-flex-center font-white sc-gap-32"
-          >
-            {renderizarBotoes(15)}
-          </div>
-        ))}
-      </div>
-      <div className="containerBotoes font-white sc-gap-32">
-        {[...Array(1)].map((_, index) => (
-          <div
-            key={index}
-            className="linhaBotoes sc-flex-center font-white sc-gap-32"
-          >
-            {renderizarBotoes(15)}
-          </div>
-        ))}
+    <div className="containerCentralizadoBeneficios">
+      <div className="bgContainerBeneficios">
+        <div className="containerBotoes sc-flex-center font-white sc-gap-32">
+          {[...Array(1)].map((_, index) => (
+            <div
+              key={index}
+              className="linhaBotoes sc-flex-center font-white sc-gap-32"
+            >
+              {renderizarBotoes(4)}
+            </div>
+          ))}
+        </div>
+        <div className="containerBotoes font-white sc-gap-32">
+          {[...Array(1)].map((_, index) => (
+            <div
+              key={index}
+              className="linhaBotoes sc-flex-center font-white sc-gap-32"
+            >
+              {renderizarBotoes(4)}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
