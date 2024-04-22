9ada96512ccb74c18a5c97bcf92a955a02ed77d4
Paula <eipvi@proton.me>
2024-04-22 11:47:29 -0300

    remove: routes in the Speakers component

--- src/app/palestrantes/page.jsx
+++ src/app/palestrantes/page.jsx
@@ -1,18 +1,18 @@
-import BoxPalestrantes from '@/components/(landingpage)/pages/boxpalestrantes';
+import BoxPalestrantes from "@/components/(landingpage)/pages/boxpalestrantes";
 
-import React from 'react';
+import React from "react";
 
 const Palestrantes = ({ children }) => {
   return (
     <div>
       {/* Aqui é onde você renderiza children */}
-      <div id='Palestrantes'>  {children} 
-  
-      <div>
-      <BoxPalestrantes />
+      <div id="Palestrantes">
+        {" "}
+        {children}
+        <div>
+          <BoxPalestrantes />
+        </div>
       </div>
-      </div>
-
     </div>
   );
 };
