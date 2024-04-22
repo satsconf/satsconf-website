fdb1a12187c7101b5b882542d18103ba3bb969a1
Paula <eipvi@proton.me>
2024-04-22 15:07:40 -0300

    feat: bg styling component for convidado destaque

--- src/components/(landingpage)/containers/boxprincipalpalestrantes/index.jsx
+++ src/components/(landingpage)/containers/boxprincipalpalestrantes/index.jsx
@@ -4,22 +4,26 @@ import ImagesConvidadosDestaque from "../imagesconvidadosdestaque";
 
 const BoxPrincipalPalestrantes = () => {
   return (
-    <div className="bgContainerConvidadosDestaque">
-      <div className="boxprincipalConvidadoDestaque font-white krona-one-regular ">
-        <div className="sc-mgem-16">
-          <span className="sc-fz-24 ">8 e 9 de Novembro | são Paulo</span>
-          <h2 className="sc-fz-48 ">CONHEÇA OS PALESTRANTES DA EDIÇÃO 2024</h2>
+    <div className="bgContainerImageConvidadosPage">
+      <div className="bgContainerConvidadosDestaque ">
+        <div className="boxprincipalConvidadoDestaque font-white krona-one-regular ">
+          <div className="sc-mgem-16 ">
+            <span className="sc-fz-24 ">8 e 9 de Novembro | são Paulo</span>
+            <h2 className="sc-fz-48 ">
+              CONHEÇA OS PALESTRANTES DA EDIÇÃO 2024
+            </h2>
+          </div>
+          <div className="sc-gap-16 container-btn">
+            <BtnIngresso url="https://www.example.com">
+              COMPRAR INGRESSO
+            </BtnIngresso>
+            {/* botão do container principal */}
+            <BtnNovidades url="/">RECEBER NOVIDADES</BtnNovidades>
+          </div>
         </div>
-        <div className="sc-gap-16 container-btn">
-          <BtnIngresso url="https://www.example.com">
-            COMPRAR INGRESSO
-          </BtnIngresso>
-          {/* botão do container principal */}
-          <BtnNovidades url="/">RECEBER NOVIDADES</BtnNovidades>
-        </div>
-       
+
+        <ImagesConvidadosDestaque />
       </div>
-      <ImagesConvidadosDestaque />
     </div>
   );
 };
