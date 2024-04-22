c48d8e9611f6db1aa2a3e7b3842d36e25a846536
Paula <eipvi@proton.me>
2024-04-22 12:03:18 -0300

    docs: repository name

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
 import BoxBeneficios from "@/components/(landingpage)/containers/boxbeneficios";
-
 import BoxCronograma from "@/components/(landingpage)/containers/boxcronograma";
-
 import BoxGarante from "@/components/(landingpage)/containers/boxgarante";
 import BoxParceiros from "@/components/(landingpage)/containers/boxparceiros";
 import BoxPatrocinadores from "@/components/(landingpage)/containers/boxpatrocinadores";
-import BoxPrincipal from "@/components/(landingpage)/containers/boxprincial";
+import BoxPrincipal from "@/components/(landingpage)/containers/boxprincipal";
 import BoxSobre from "@/components/(landingpage)/containers/boxsobre";
 import ConvidadosPaginaPrincipal from "@/components/(landingpage)/containers/convidadospaginaprincipal";
 import FAQComponent from "@/components/(landingpage)/containers/faq";
