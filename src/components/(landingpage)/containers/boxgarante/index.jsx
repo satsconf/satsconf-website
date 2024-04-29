9beba472d90c89fb3f7be71f026223f9a7eb4b39
Paula <eipvi@proton.me>
2024-04-29 10:02:52 -0300

    fix: adjustment in the styling of the button component ensures
    Problem related to button responsiveness

--- src/components/(landingpage)/containers/boxgarante/index.jsx
+++ src/components/(landingpage)/containers/boxgarante/index.jsx
@@ -5,6 +5,7 @@ import { FiCheckCircle } from "react-icons/fi";
 import MeuComponenteLogo from "@/components/(landingpage)/images/MeuComponenteLogo";
 import MeuComponenteIgressoVip from "@/components/(landingpage)/images/ImageLogoVip";
 import BtnCronograma from "@/components/(landingpage)/ui/button/BtnCronograma";
+import BtnCronogramaSecundary from "../../ui/button/BtnCronogramaSecundary/inde";
 
 
 const BoxGarante = () => {
@@ -16,70 +17,86 @@ const BoxGarante = () => {
         <div className="boxGarante">
           <div className="boxItem boxColorPrimary">
             <span className="krona-one-regular ">2 A 4 NOVEMBRO</span>
-            <div className="boxLogoGarante">
+            <div className="boxLogoGarante imagensLogoConatinerSite">
               <MeuComponenteLogo />
             </div>
 
             <ul className="boxListGarante">
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso ao Palco Nakamoto
+                <FiCheckCircle size={38} color="white" /> Acesso ao Palco Nakamoto
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso ao Palco Hal Finney
+                <FiCheckCircle size={38} color="white" /> Acesso ao Palco Hal Finney
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso às salas de Workshop
+                <FiCheckCircle size={38} color="white" /> Acesso às salas de Workshop
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso à Área de Exposição
+                <FiCheckCircle size={38} color="white" /> Acesso à Área de Exposição
               </li>
 
             </ul>
+            <div className="containerbtnHome marginBottonHomeCronograma">
             <BtnCronograma url=""> ESGOTADO</BtnCronograma>
+            </div>
+    
           </div>
 
           <div className="boxItemTwo boxColorSecundary">
             <span className="krona-one-regular ">2 A 6 NOVEMBRO</span>
-            <di className="boxLogoGarante">
+            <div className="boxLogoGarante imagensLogoConatinerSite">
               <MeuComponenteIgressoVip />
-            </di>
+            </div>
 
             <ul className="boxListGarante">
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso ao Palco Nakamoto
+                <FiCheckCircle size={38} color="white" /> 
+                <p>Acesso ao Palco Nakamoto </p>
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso ao Palco Hal Finney
+                <FiCheckCircle size={38} color="white" />
+                <p>Acesso ao Palco Hal Finney</p> 
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso às salas de Workshop
+                <FiCheckCircle size={38} color="white" /> 
+                <p>Acesso às salas de Workshop</p>
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" />Acesso à Área de Exposição
+                <FiCheckCircle size={38} color="white" />
+                <p> Acesso à Área de Exposição</p>
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Entrada especial no evento
+                <FiCheckCircle size={38} color="white" /> 
+                <p>Entrada especial no evento</p>
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Assentos VIP na plateia de todas as palestras*
+                <FiCheckCircle size={38} color="white" /> 
+                <p>Assentos VIP na plateia de todas as palestras*</p>
               </li>  <li>
-                <FiCheckCircle size={20} color="white" /> Bitcoffe Break com café e snacks**
+                <FiCheckCircle size={38} color="white" /> 
+                <p>Bitcoffe Break com café e snacks**</p>
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Espaço VIP com benefícios e surpresas
+                <FiCheckCircle size={38} color="white" /> 
+                <p> Espaço VIP com benefícios e surpresas</p>
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso privilegiado aos palestrantes
+                <FiCheckCircle size={38} color="white" /> 
+                <p> Acesso privilegiado aos palestrantes</p>
               </li>
               <li>
-                <FiCheckCircle size={20} color="white" /> Acesso a SATSPARTY open bar e open food
+                <FiCheckCircle size={38} color="white" /> 
+                <p>Acesso a SATSPARTY open bar e open food</p>
               </li>
             </ul>
             <div className="boxListGaranteInfo">
               <p>*Números de assentos VIP limitados e sujeito a alteração.</p>
 
             </div>
-            <BtnCronograma url=""> COMPRAR INGRESSO AGORA</BtnCronograma>
+            <div className="containerbtnHome marginBottonHomeCronogramaTwo">
+            <BtnCronogramaSecundary  url="" > COMPRAR INGRESSO AGORA </BtnCronogramaSecundary>
+
+            </div>
           </div>
         </div>
       </div>
