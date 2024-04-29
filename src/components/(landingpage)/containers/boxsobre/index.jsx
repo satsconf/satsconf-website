54aca92c4f31af5fa6cd0475eb7dd8b07ffc3c0f
Paula <eipvi@proton.me>
2024-04-29 16:49:10 -0300

    fix:  adjustment to the transition in the over component

--- src/components/(landingpage)/containers/boxsobre/index.jsx
+++ src/components/(landingpage)/containers/boxsobre/index.jsx
@@ -1,73 +1,71 @@
-"use client"
+"use client";
 
 import { FiPlusCircle, FiMinusCircle } from "react-icons/fi";
 
-import { useState, useEffect } from 'react';
-  
+import { useState, useEffect } from "react";
 
 const BoxSobre = () => {
+  const [selectedIcon, setSelectedIcon] = useState(null);
 
-    const [selectedIcon, setSelectedIcon] = useState(null);
+  useEffect(() => {
+    // Define o primeiro item como selecionado inicialmente quando a página é carregada
+    setSelectedIcon(0);
+  }, []);
 
-    useEffect(() => {
-        // Define o primeiro item como selecionado inicialmente quando a página é carregada
-        setSelectedIcon(0);
-    }, []);
+  const toggle = (i) => {
+    setSelectedIcon(selectedIcon === i ? null : i);
+  };
 
+  return (
+    <div className="bgContainerSobre ">
+      <div className="grid-container font-white ">
+        <div className="block">
+          <h2 className="sc-fz-18">SOBRE A SATS CONF</h2>
 
-    const toggle = (i) => {
-        setSelectedIcon(selectedIcon === i ? null : i);
-    };
-
-
-    return (
-        <div className="bgContainerSobre ">
-            <div className="grid-container font-white ">
-                <div className="block">
-                <h2 className="sc-fz-18">SOBRE A SATS CONF</h2>
-
-                {data.map((item, i) => {
-                   return (
-                    <div className="item" key={i}>
-                        <div className="title" onClick={() => toggle(i)}>
-                            <h3>{item.question}</h3>
-                            <span className="icon-wrapper">
-                            {selectedIcon === i ? <FiMinusCircle className="icon" /> : <FiPlusCircle className="icon" />}
-                                </span>
-                        </div>
-                        <div className="content" style={{ display: selectedIcon === i ? 'flex' : 'none' }}>{item.answer}</div>
-                    </div>
-                );
-                })}
+          {data.map((item, i) => {
+            return (
+              <div className="item" key={i}>
+                <div className="title" onClick={() => toggle(i)}>
+                  <h3>{item.question}</h3>
+                  <span className="icon-wrapper">
+                    {selectedIcon === i ? (
+                      <FiMinusCircle className="icon" />
+                    ) : (
+                      <FiPlusCircle className="icon" />
+                    )}
+                  </span>
                 </div>
-                <div className="containerVideo">
-               
-                <p>Video</p>
-
-                
+                <div className={`content ${selectedIcon === i ? "open" : ""}`}>
+                  {item.answer}
                 </div>
-   
-            </div>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+        <div className="containerVideo">
+          <p>Video</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const data = [
-    {
-        question:'O que é a Satsconf?',
-            answer:
-            'A Satsconf é um evento que reúne as maiores referências nacionais e internacionais do Bitcoin para compartilhar conhecimento sobre liberdade, soberania individual e a internet do dinheiro.',
-    },
-    {
-        question:'Pra quem é?',
-        answer:
-        'A Satsconf é um evento feito para todos aqueles que têm interesse em aprender mais sobre o Bitcoin, se conectar com a comunidade e explorar temas como liberdade, soberania individual e a evolução da internet do dinheiro. É uma oportunidade única de absorver conhecimento de especialistas nacionais e internacionais, além de estabelecer conexões valiosas para o seu crescimento pessoal e profissional.',
-    },
-    {
-        question:'Por que ir a Satsconf?',
-        answer:
-        'Ir à Satsconf é entrar em um universo vibrante do Bitcoin, com especialistas e entusiastas. Lá, você expande seu conhecimento sobre o ativo, explora as tendências mais recentes e contribui para o futuro financeiro digital. É o ponto de encontro ideal para networking, conexões valiosas e inspiração em finanças descentralizadas. Se quer atualização, networking e imersão na comunidade Bitcoin, a Satsconf é o lugar.',
-    }
-]
+  {
+    question: "O que é a Satsconf?",
+    answer:
+      "A Satsconf é um evento que reúne as maiores referências nacionais e internacionais do Bitcoin para compartilhar conhecimento sobre liberdade, soberania individual e a internet do dinheiro.",
+  },
+  {
+    question: "Pra quem é?",
+    answer:
+      "A Satsconf é um evento feito para todos aqueles que têm interesse em aprender mais sobre o Bitcoin, se conectar com a comunidade e explorar temas como liberdade, soberania individual e a evolução da internet do dinheiro. É uma oportunidade única de absorver conhecimento de especialistas nacionais e internacionais, além de estabelecer conexões valiosas para o seu crescimento pessoal e profissional.",
+  },
+  {
+    question: "Por que ir a Satsconf?",
+    answer:
+      "Ir à Satsconf é entrar em um universo vibrante do Bitcoin, com especialistas e entusiastas. Lá, você expande seu conhecimento sobre o ativo, explora as tendências mais recentes e contribui para o futuro financeiro digital. É o ponto de encontro ideal para networking, conexões valiosas e inspiração em finanças descentralizadas. Se quer atualização, networking e imersão na comunidade Bitcoin, a Satsconf é o lugar.",
+  },
+];
 
 export default BoxSobre;
