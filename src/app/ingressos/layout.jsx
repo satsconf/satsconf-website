edbaa5086c685b0ee962b419fc9c56fcc460d899
Paula <eipvi@proton.me>
2024-05-01 15:59:31 -0300

    :sparkles: Ticket page and main container code.

---
+++ src/app/ingressos/layout.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: "Ingressos",
+    description: "Veja os ingressos do Satsconf 2024",
+  };
+  
+
+export default function IngressosLayout({ children }) {
+    return (
+      <>
+      {children}
+      </>
+    )
+  }
