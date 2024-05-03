b16fefd70f3d9667e0f1be8225b7b931efb4febd
Paula <eipvi@proton.me>
2024-05-03 15:43:33 -0300

    :sparkles: Entry table component of the ticket page.

---
+++ src/components/(landingpage)/pages/ingresso/boxtabelaingresso/index.jsx
@@ -0,0 +1,94 @@
+import BtnIngresso from "@/components/(landingpage)/ui/button/BtnIngresso";
+import BtnNovidades from "@/components/(landingpage)/ui/button/BtnNovidades";
+import { FiCheckCircle, FiMinus } from "react-icons/fi";
+
+const BoxTabelaIngresso = () => {
+  return (
+    <div className="bgTabelaIngresso">
+      <div className="boxTitle">
+        <h2>O QUE ESTÁ INCLUSO</h2>
+      </div>
+      <table className="BoxTabelaIngresso sora-regular sc-fz-18">
+        <thead>
+          <tr>
+            <th className="bordaNone"></th>
+            <th className="boxCenter bordaNone">
+              <img
+                src="/ingresso-normal-satsconf.png"
+                alt="ingresso normal satsconf"
+              />
+            </th>
+            <th className="boxCenter bordaNone">
+              <img
+                src="/ingresso-vip-satsconf.png"
+                alt="ingresso VIP satsconf"
+              />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="bordaleft">Acesso a palestras do Palco x</td>
+            <td className="boxCenter">
+              {" "}
+              <FiCheckCircle size={38} color="white" />{" "}
+            </td>
+            <td className="boxCenter">
+              {" "}
+              <FiCheckCircle size={38} color="white" />
+            </td>
+          </tr>
+          <tr>
+            <td className="bordaleft">Acesso a palestras do Palco x</td>
+            <td className="boxCenter">
+              <FiMinus size={38} color="white" />
+            </td>
+            <td className="boxCenter">
+              {" "}
+              <FiCheckCircle size={38} color="white" />
+            </td>
+          </tr>
+          <tr>
+            <td className="bordaleft">Acesso a palestras do Palco x</td>
+            <td className="boxCenter">
+              <FiMinus size={38} color="white" />
+            </td>
+            <td className="boxCenter">
+              {" "}
+              <FiCheckCircle size={38} color="white" />
+            </td>
+          </tr>
+          <tr>
+            <td className="bordaleft">Acesso a palestras do Palco x</td>
+            <td className="boxCenter">
+              <FiMinus size={38} color="white" />
+            </td>
+            <td className="boxCenter">
+              {" "}
+              <FiCheckCircle size={38} color="white" />
+            </td>
+          </tr>
+          <tr>
+            <td className="bordaleft">Acesso a palestras do Palco x</td>
+            <td className="boxCenter">
+              <FiMinus size={38} color="white" />
+            </td>
+            <td className="boxCenter">
+              {" "}
+              <FiCheckCircle size={38} color="white" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="sc-gap-8 containerBtnIngresso">
+        <BtnIngresso url="https://www.example.com">
+          COMPRAR INGRESSO
+        </BtnIngresso>
+        {/* botão do container principal */}
+        <BtnNovidades url="/">COMPRAR EM GRUPO</BtnNovidades>
+      </div>
+    </div>
+  );
+};
+
+export default BoxTabelaIngresso;
