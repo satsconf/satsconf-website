a113d3ef59a30b054ad18829674ab050170f9cf1
Paula <eipvi@proton.me>
2024-05-06 20:54:02 -0300

    :sparkles: Images and Components

---
+++ src/components/(landingpage)/images/ImageFaq/index.jsx
@@ -0,0 +1,13 @@
+const ImageFaq = () => {
+    return (
+      <img
+        src="./duvidas-sobre-satsconf-evento.png"
+        width="374"
+        height="374"
+        alt="Imagem do Evento Satsconf de 2023"
+      />
+    );
+  };
+  
+  export default ImageFaq;
+  
---
+++ src/components/(landingpage)/images/Satsparty/imagePrincipalSatsParty/index.jsx
@@ -0,0 +1,13 @@
+const ImagePrincipalSatsParty = () => {
+    return (
+      <img
+        src="./satsparty-satsconf-evento-sao-paulo.png"
+        width="374"
+        height="374"
+        alt="Imagem do Evento Satsconf de 2023"
+      />
+    );
+  };
+  
+  export default ImagePrincipalSatsParty;
+  
