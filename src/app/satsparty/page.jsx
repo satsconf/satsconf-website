84928c07f3bbbd51f7caa205c236568f24b88070
Paula <eipvi@proton.me>
2024-05-13 11:49:23 -0300

    ✨ :sparkles: Creation of the components for the SATS Conf Party webpage.
    Development of the page featuring images from the 2023 event, including styling, responsiveness, and background design.

--- src/app/satsparty/page.jsx
+++ src/app/satsparty/page.jsx
@@ -1,14 +1,23 @@
 import ImagePrincipalSatsParty from "@/components/(landingpage)/images/Satsparty/imagePrincipalSatsParty";
+import SatsPartyImgEvento from "@/components/(landingpage)/images/Satsparty/satspartyEvento";
+import BtnIngresso from "@/components/(landingpage)/ui/button/BtnIngresso";
+import BtnNovidades from "@/components/(landingpage)/ui/button/BtnNovidades";
 
 const pSatsParty = ({ children }) => {
   return (
     <div id="pSatsParty">
+      <div className="bgContainerPrincipalImg">
       <div className="bgContainerPrincipal ">
         <div className=" bgContainerMargin">
           <div className="boxprincipal">
             {/* caixa co container com fonte do titulo */}
             <div className="sc-bg-container krona-one-regular ">
-              <h1>TIRE SUAS DÚVIDAS SOBRE A SATSCONF</h1>
+              <span className="SatsConfParty-Span">Evento exclusivo para vips</span>
+              <h1 className="margem-top-30">ENTRE NO NOVO MUNDO COM A GENTE!</h1>
+              <div className="container-btn-satsparty margem-top-20">
+              <BtnIngresso url="/">COMPRAR INGRESSO</BtnIngresso>
+              </div>
+        
               {/* gap de 16 com container de botão */}
             </div>
 
@@ -19,9 +28,11 @@ const pSatsParty = ({ children }) => {
           </div>
         </div>
       </div>
-      <div className=" krona-one-regular">
-        <div></div>
+    
+        <div> <SatsPartyImgEvento /></div>
       </div>
+     
+      
     </div>
   );
 };
---
+++ src/components/(landingpage)/images/Satsparty/satspartyEvento/index.jsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import BtnIngresso from "@/components/(landingpage)/ui/button/BtnIngresso";
+
+const SatsPartyImgEvento = () => {
+  return (
+    <div className="BoxSatsParty">
+      <div className="ContainerSatsPartyEvento">
+        <div className="gallerySatsParty  ">
+          <div className=" img1">
+            <img
+              src="/evento-bitcoin-satsconf-mulheres-bitconheiras.png"
+              alt="Image 1"
+            />
+          </div>
+          <div className=" img2">
+            <img src="/evento-bitcoin-satsconf-sao-paulo.png" alt="Image 2" />
+          </div>
+          <div className=" img4">
+            <img src="/evento-bitcoin-sao-paulo.png" alt="Image 4" />
+          </div>
+          <div className=" img5">
+            <img src="/maior-evento-bitcoin-brasil.png" alt="Image 5" />
+          </div>
+          <div className=" img6">
+            <img src="/bitcoin-evento-satsconf.png" alt="Image 6" />
+          </div>
+          <div className=" img7">
+            <img src="/bitcoin-evento.png" alt="Image 7" />
+          </div>
+          <div className=" img8">
+            <img src="/evento-bitcoin-brasil.png" alt="Image 8" />
+          </div>
+          <div className=" img9">
+            <img src="/evento-satsconf-saiba-mais.png" alt="Image 9" />
+          </div>
+          <div className=" img10">
+            <img src="/comprar-ingresso-bitcoin.png" alt="Image 10" />
+          </div>
+          <div className=" img11">
+            <img src="/comprar-ingresso-bitcoin.png" alt="Image 11" />
+          </div>
+          <div className=" img12">
+            <img src="/quem-somos-nos-satsconf.png" alt="Image 12" />
+          </div>
+          <div className=" img13">
+            <img src="/satsconf-evento-brazil.png" alt="Image 13" />
+          </div>
+       
+          <div className=" img14">
+            <img
+              src="/sats-evento-bitcoin-sp-brazil-sats-conf-evento-bitcoin.png"
+              alt="Image 14"
+            />
+          </div>
+          <div className=" img15">
+            <img
+              src="/sats-evento-bitcoin-sp-brazil-sats-conf-evento-bitcoin.png"
+              alt="Image 15"
+            />
+          </div>
+        </div>
+        <div className=" img16 itemSatsPartyEvento ">
+          <div className="container-div-bottom-satsparty">
+            <h4>
+              FAÇA PARTE DESSE MOMENTO NESTA EDIÇÃO, GARANTA AGORA SEU INGRESSO
+              VIP
+            </h4>
+            <div className="container-btn-satsparty">
+              <BtnIngresso url="/">COMPRAR SEU INGRESSO VIP AGORA </BtnIngresso>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SatsPartyImgEvento;
