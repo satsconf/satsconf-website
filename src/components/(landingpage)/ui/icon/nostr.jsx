688bcc17d35827a639411e771eebd5a61650340e
Paula <eipvi@proton.me>
2024-05-15 08:09:54 -0300

    ✨ icon nostr

---
+++ src/components/(landingpage)/ui/icon/nostr.jsx
@@ -0,0 +1,10 @@
+const IconNostr = () => {
+    return (
+      <svg width="26" height="26" >
+        <image className="iconNostr" href="/iconnostr.png" width="30px" height="30px" alt="Nostr" />
+      </svg>
+    );
+  };
+  
+  export default IconNostr;
+  
