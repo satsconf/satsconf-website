9361cf6a1eee4787284e03dc8cadf3b925faf177
Paula <eipvi@proton.me>
2024-05-16 14:33:13 -0300

    📦  Testing new dependencies.

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	env: {
+		SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+		SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
+	},
+    images: {
+        domains: ['cdn.shopify.com'],
+    },
+}
+	
