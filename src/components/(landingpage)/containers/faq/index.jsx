bd429ab96c1ac0bec401c64cfffbc33779a0e03c
Paula <eipvi@proton.me>
2024-05-17 16:10:46 -0300

    💄 FAQ component tuning

--- src/components/(landingpage)/containers/faq/index.jsx
+++ src/components/(landingpage)/containers/faq/index.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FiPlusCircle, FiMinusCircle } from "react-icons/fi";
+import { IoMdAdd, IoMdClose } from "react-icons/io";
 import { useState } from "react";
 
 const FAQ = () => {
@@ -16,26 +16,23 @@ const FAQ = () => {
         <h2 className="containerTitleFaq">FAQ</h2>
       </div>
       {data.map((item, index) => (
-        <div className="faqItem " key={index}>
-          <div className="faqHeader">
-            <div className="title" onClick={() => toggleItem(index)}>
+        <div className={`faqItem ${selectedItem === index ? 'open' : ''}`} key={index}>
+          <div className="faqHeader" onClick={() => toggleItem(index)}>
+            <div className="title">
               <span className="faqNumber" id={`faq-${index}`}>
                 {index + 1}
               </span>
               <h3 className="faqQuestion">{item.question}</h3>
-              <span className="faqToggle">
+              <span className={`faqToggle ${selectedItem === index ? 'open-icon' : ''}`}>
                 {selectedItem === index ? (
-                  <FiMinusCircle className="icon" />
+                  <IoMdClose className="icon" />
                 ) : (
-                  <FiPlusCircle className="icon" />
+                  <IoMdAdd className="icon" />
                 )}
               </span>
             </div>
-            <div
-              className="faqAnswer"
-              style={{ display: selectedItem === index ? "block" : "none" }}
-            >
-              {item.answer}
+            <div className="faqAnswer" style={{ display: selectedItem === index ? "block" : "none" }}>
+               {item.answer}
             </div>
           </div>
         </div>
