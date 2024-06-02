8696c0043e2c5792fee12ea9e2bd0580ee597292
Paula <eipvi@proton.me>
2024-06-02 20:46:56 -0300

    🗑️ repeated component

--- src/app/palestrantes/page.jsx
+++ src/app/palestrantes/page.jsx
@@ -1,4 +1,6 @@
-import BoxPrincipalPalestrantes from "@/components/(landingpage)/pages/boxpalestrantes/boxprincipalpalestrantes";
+import BtnIngresso from "@/components/(landingpage)/ui/button/BtnIngresso";
+import BtnNovidades from "@/components/(landingpage)/ui/button/BtnNovidades";
+import Card from "@/components/(landingpage)/ui/modal/modalDestaque/profilecard";
 
 const Palestrantes = ({ children }) => {
 
@@ -7,7 +9,30 @@ const Palestrantes = ({ children }) => {
     <div id="PagPalestrantes" >
       {" "}
       {children}
-    <BoxPrincipalPalestrantes />
+      <section className="bgContainerImageConvidadosPage">
+      <div className="bgContainerConvidadosDestaque ">
+        <div className="boxprincipalConvidadoDestaque font-white krona-one-regular ">
+          <div className="sc-mgem-16 ">
+            <span className="sc-fz-24 ">8 e 9 de Novembro | são Paulo</span>
+            <h2 className="sc-fz-48 ">
+              CONHEÇA OS PALESTRANTES DA EDIÇÃO 2024
+            </h2>
+          </div>
+          <div className="sc-gap-8 container-btn">
+            <BtnIngresso url="https://www.example.com">
+              COMPRAR INGRESSO
+            </BtnIngresso>
+            {/* botão do container principal */}
+            <BtnNovidades url="/">RECEBER NOVIDADES</BtnNovidades>
+          </div>
+        </div>
+        <div>
+        <Card />
+       
+        </div>
+     
+      </div>
+    </section>
     </div>
   );
 };
