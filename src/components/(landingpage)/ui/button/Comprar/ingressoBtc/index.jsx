d972a4114544065923a0bc3668bfaafc9772d5e6
Paula <eipvi@proton.me>
2024-06-04 11:37:06 -0300

    💄 Portuguese adjustment

--- src/components/(landingpage)/ui/button/Comprar/ingressoBtc/index.jsx
+++ src/components/(landingpage)/ui/button/Comprar/ingressoBtc/index.jsx
@@ -40,7 +40,7 @@ const CompraBtc = () => {
       </ul>
 
       <div className="boxDescontoIngresso">
-        <h3>ATENÇÃO! 21% de desconto para pagando em Bitcoin!</h3>
+        <h3>ATENÇÃO! 21% de desconto pagando em Bitcoin!</h3>
       </div>
     </div>
   );
