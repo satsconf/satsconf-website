69f3bbc5f8cd98604f43863ba7a3cba5d4826d07
Paula <eipvi@proton.me>
2024-06-20 15:30:09 -0300

    ✨ function to exit navigation when clicking on the link

--- src/components/(landingpage)/navbar/index.jsx
+++ src/components/(landingpage)/navbar/index.jsx
@@ -62,13 +62,13 @@ const toggleDropdownTwo = () => {
               <button href="/" onClick={toggleDropdown} className="dropbtn navItem">PROGRAMAÇÃO</button>
             </li>
             <div className={`dropdown-content ${showDropdown ? "show" : ""}`}>
-            <Link href="/programacao">Agenda 2024</Link>
-            <Link href="/programacao">SatsWeek</Link>
-            <Link href="/programacao">SatsArte</Link>
-            <Link href="/programacao">SatsGaming</Link>
-            <Link href="/programacao">SatsKids</Link>
-            <Link href="/programacao">SatsParty</Link>
-            <Link href="/programacao">SatsMarket</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>Agenda 2024</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>SatsWeek</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>SatsArte</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>SatsGaming</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>SatsKids</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>SatsParty</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>SatsMarket</Link>
             </div>
             </div>
             <div className="dropdown">
@@ -76,11 +76,11 @@ const toggleDropdownTwo = () => {
               <button href="/" onClick={toggleDropdownTwo} className="dropbtn navItem">MAIS</button>
             </li>
             <div className={`dropdown-content ${showDropdownT ? "show" : ""}`}>
-            <Link href="/programacao">Ingresso Estudante</Link>
-            <Link href="/programacao">Imprensa</Link>
-            <Link href="/programacao">Patrocínio</Link>
-            <Link href="/programacao">Hotéis</Link>
-            <Link href="/programacao">Local do Evento</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>Ingresso Estudante</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>Imprensa</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>Patrocínio</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>Hotéis</Link>
+            <Link href="/programacao" onClick={handleLinkClickHome}>Local do Evento</Link>
             <Link href="/faq" onClick={handleLinkClickHome}>Faq</Link>
             </div>
             </div>
