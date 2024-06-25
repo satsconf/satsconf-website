496ba06a75383a940dd9f42780994ef68ec05c95
Paula <eipvi@proton.me>
2024-06-25 17:45:54 -0300

    style: insert modal on the programming page

--- src/app/programacao/page.jsx
+++ src/app/programacao/page.jsx
@@ -1,13 +1,25 @@
+"use client";
+import React from "react";
+import { useContext } from "react";
+import { ModalContext } from "@/providers/ModalContext";
+import BtnNovidades from "@/components/(landingpage)/ui/button/BtnNovidades";
 
 const Programacao = ({ children }) => {
+  const { handleModalVisible } = useContext(ModalContext);
+
+  const handleOpenModal = () => {
+    handleModalVisible();
+  };
+
   return (
     <section id="PagProgramacao">
-            {/* Aqui é onde você renderiza children */} {children}
-        <div className="containerPagProgramacao">
-            <h2>Em breve mais informações!</h2>
-            <p>Estamos preparando algo especial para você!</p>
-        </div>
-      </section>
+      {/* Aqui é onde você renderiza children */} {children}
+      <div className="containerPagProgramacao">
+        <h2>Em breve mais informações!</h2>
+        <p>Estamos preparando algo especial para você!</p>
+        <BtnNovidades onClick={handleOpenModal}>Seja notificado!</BtnNovidades>
+      </div>
+    </section>
   );
 };
 
