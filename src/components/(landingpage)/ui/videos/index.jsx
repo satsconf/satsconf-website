3e605550b866ca3ef9aacba5d90cdfe1492fbed4
Paula <eipvi@proton.me>
2024-07-08 09:02:59 -0300

    feat: adjustment for video component error

--- src/components/(landingpage)/ui/videos/index.jsx
+++ src/components/(landingpage)/ui/videos/index.jsx
@@ -1,34 +1,42 @@
 "use client"
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const PlaySatsconf = () => {
   const videoRef = useRef(null);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    setIsClient(true);
     if (videoRef.current) {
       videoRef.current.play();
     }
   }, []);
 
   return (
-    <video   
-    width="235" 
-    height="420" 
-    controls 
-    preload="auto"
-    autoPlay
-    loop
-    muted
-    >
-      <source src="/satsconf-video.mp4" type="video/mp4" />
-      <track
-        src="/satsconf-video.mp4"
-        kind="subtitles"
-        srcLang="en"
-        label="English"
-      />
-      Your browser does not support the video tag.
-    </video>
+    <>
+    {isClient && (
+      <video
+        ref={videoRef}
+        width="235"
+        height="420"
+        controls
+        preload="auto"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/satsconf-video.mp4" type="video/mp4" />
+        <track
+          src="/satsconf-video.mp4"
+          kind="subtitles"
+          srcLang="en"
+          label="English"
+        />
+        Your browser does not support the video tag.
+      </video>
+    )}
+  </>
+    
   )
 }
 
