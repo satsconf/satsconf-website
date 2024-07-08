372f99e1212e2a563661651a134a88d486a8e896
Paula <eipvi@proton.me>
2024-07-08 08:36:02 -0300

    feat: new video component and style adjustment

---
+++ src/components/(landingpage)/ui/videos/index.jsx
@@ -0,0 +1,35 @@
+"use client"
+import { useEffect, useRef } from 'react';
+
+const PlaySatsconf = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  }, []);
+
+  return (
+    <video   
+    width="235" 
+    height="420" 
+    controls 
+    preload="auto"
+    autoPlay
+    loop
+    muted
+    >
+      <source src="/satsconf-video.mp4" type="video/mp4" />
+      <track
+        src="/satsconf-video.mp4"
+        kind="subtitles"
+        srcLang="en"
+        label="English"
+      />
+      Your browser does not support the video tag.
+    </video>
+  )
+}
+
+export default PlaySatsconf;
