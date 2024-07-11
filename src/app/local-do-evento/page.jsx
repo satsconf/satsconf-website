5be5fcb75b398b4b8f9190db09cdf1aa4701c18e
Paula <eipvi@proton.me>
2024-07-11 18:19:01 -0300

    feat: adjustment of event location page component

---
+++ src/app/local-do-evento/page.jsx
@@ -0,0 +1,44 @@
+import ImageLocal from "@/components/(landingpage)/images/ImageContato";
+import Mapa from "@/components/(landingpage)/pages/localevento/mapa";
+
+const LocalEvento = ({ children }) => {
+  return (
+    <section id="LocalEvento">
+      {/* Aqui é onde você renderiza children */}
+      {children}
+      <div className="bgContainerImgFaq">
+        <div className="bgContainerPrincipal">
+          <div className=" bgContainerMargin ">
+            <div className="boxprincipal">
+              {/* caixa co container com fonte do titulo */}
+              <div className="sc-bg-container krona-one-regular divFontLocalEvento lineTitle-50">
+                <span>8 e 9 de Novembro</span>
+                <h1>AUDIO CLUB SÃO PAULO</h1>
+                <h3>Av Francisco Matarazzo, 694</h3>
+                {/* gap de 16 com container de botão */}
+              </div>
+
+              <div className="btningresso-img container-com-gradiente">
+                {/* imagem do container principal esportado como componente */}
+                <ImageLocal />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="containerLocal">
+        <div className="boxTitle">
+            {" "}
+            <h2 className=" containerTitleForm">
+            Como chegar
+            </h2>
+
+          </div>
+          
+        <Mapa />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LocalEvento;
