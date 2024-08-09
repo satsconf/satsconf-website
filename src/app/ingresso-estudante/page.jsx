3e9d5da576c773aa40a8140e9064fe286bec6b24
Paula <eipvi@proton.me>
2024-08-09 19:23:39 -0300

    feat ajust error img, link

--- src/app/ingresso-estudante/page.jsx
+++ src/app/ingresso-estudante/page.jsx
@@ -21,55 +21,63 @@ const IngressoEstudante = ({ children }) => {
                 {/* gap de 8 com container de botão */}
               </div>
 
-              <div className="btningresso-img container-com-gradiente " >
+              <div className="btningresso-img container-com-gradiente ">
                 {/* imagem do container principal da pagina de ingresso esportado como componente */}
                 <ImageEstudante />
               </div>
             </div>
           </div>
         </div>
-      
-      <div className="degraderBlack">
-      <div className="bgContainerComprarIngresso">
-        <div className="containerEstudante">
-          <div className="textEstudante">
-            <p>{t("student.textOne")}</p>
-            <br />
-            <p>{t("student.textTwo")}</p>
-            <img src="./logoBitcoinStudentsNetwork.svg" alt="" />
-            <br />
-            <p>
-              <Trans i18nKey="student.textThree">
-                <a href="https://www.genbitcoin.org/"></a>
-              </Trans>
-            </p>
-            <br />
-            <p>{t("student.textFour")}</p>
-            <div className=" margem-top-30 containerComprarIngressoestudante">
-              <ul>
-                <li>
+
+        <div className="degraderBlack">
+          <div className="bgContainerComprarIngresso">
+            <div className="containerEstudante">
+              <div className="textEstudante">
+                <p>{t("student.textOne")}</p>
+                <br />
+                <p>{t("student.textTwo")}</p>
+                <Link href="https://www.bitcoinstudentsnetwork.org/">
                   <img
-                    src="estudanteIngressoNormal.png"
-                    alt="Comprar ingresso Normal Pass"
+                    src="./logoBitcoinStudentsNetwork.svg"
+                    alt="Bitcoin Students Network"
                   />
-                </li>
-                <li>
-                  <span>{t("student.ticket")}</span>
-                </li>
-                <li className="krona-one-regular">R$50</li>
-                <li>
-                  <div className="buttonIngressoComprar">
-                    <Link href="https://ingresso.satsconf.com.br/cart/41822036721799:1?storefront=true">
-                      {t("buttons.purchaseNow")}
-                    </Link>
-                  </div>
-                </li>
-              </ul>
+                </Link>
+
+                <br />
+                <p>
+                  <Trans i18nKey="student.textThree">
+                    <a href="https://www.genbitcoin.org/"></a>
+                  </Trans>
+                </p>
+                <br />
+                <p>{t("student.textFour")}</p>
+                <br />
+                <p>{t("student.textFive")}</p>
+                <div className=" margem-top-30 containerComprarIngressoestudante">
+                  <ul>
+                    <li>
+                      <img
+                        src="estudanteIngressoNormal.png"
+                        alt="Comprar ingresso Normal Pass"
+                      />
+                    </li>
+                    <li>
+                      <span>{t("student.ticket")}</span>
+                    </li>
+                    <li className="krona-one-regular">R$50</li>
+                    <li>
+                      <Link href="https://ingresso.satsconf.com.br/cart/41822036721799:1?storefront=true">
+                        <div className="buttonIngressoComprar">
+                          {t("buttons.purchaseNow")}
+                        </div>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      </div>
       </div>
     </section>
   );
