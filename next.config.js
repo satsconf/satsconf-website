8f8d51abbf50102d2e2c07b2df11a6e29d0cec57
jaonoctus <jaonoctus@protonmail.com>
2024-08-28 23:42:36 -0300

    fix: setup cors for nip05

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
     defaultLocale: 'pt-BR',
     localeDetection: false, // Desativa a detecção automática do Next.js para caminho baseado em idioma
   },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }
+        ]
+      }
+    ]
+  }
 }
