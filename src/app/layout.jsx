12ce0812e69ef0d4f44230e5f59d02c5477a8ac1
Paula <eipvi@proton.me>
2024-09-20 09:23:17 -0300

    feat ajust erro

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -43,8 +43,14 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ6K67B"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-PJ6K67B"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  ></iframe>
+</noscript>
         {isMaintenanceMode && isHomePage ? (
           <Manutencao />
         ) : (
