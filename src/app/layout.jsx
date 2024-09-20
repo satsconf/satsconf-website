3d603e632b8948edf7209308f7606bd9e7abb403
Paula <eipvi@proton.me>
2024-09-19 22:50:51 -0300

    feat tag manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,6 +25,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
+           {/* Google Tag Manager script */}
+<script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PJ6K67B');`,
+          }}
+        />
+
+
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description}></meta>
         <meta name="keywords" content={metadata.keywords}></meta>
@@ -40,6 +51,12 @@ export default function RootLayout({ children }) {
         ))}
       </head>
       <body>
+      {/* Google Tag Manager (noscript) */}
+<noscript>
+  <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ6K67B"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+
         {isMaintenanceMode && isHomePage ? (
           <Manutencao />
         ) : (
